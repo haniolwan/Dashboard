3229059b4b010a72380fdfd60e2ed94d0330c5dd
Hani Olwan <49692805+haniolwan@users.noreply.github.com>
2023-05-27 17:46:07 +0300

    Show Subscription

--- src/Components/Popups/Show/Subscription/index.jsx
+++ src/Components/Popups/Show/Subscription/index.jsx
@@ -1,89 +1,63 @@
-import { TextInput } from "../../../common";
+import { useEffect, useState } from "react";
 import Form from "../../Insert/Form";
 
 const ShowSubscription = ({ show, setShow, selectedRow }) => {
+  const [subscription, setSubscription] = useState([]);
+  useEffect(() => {
+    if (selectedRow) {
+      setSubscription(selectedRow);
+    }
+  }, [selectedRow]);
+
   return (
     <Form show={show} setShow={setShow}>
       <Form.Container>
-        <Form.Content title={"Show Subscription"}>
+        <Form.Content title={"Page Info"}>
           <Form.Row className="grid grid-cols-12 gap-5">
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.name}
-                name={"name"}
-                label={"Name"}
-                defaultValue={selectedRow.name}
-                disabled
-              />
-            </div>
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.billing_days}
-                name={"billing_days"}
-                label={"Billing days"}
-                defaultValue={selectedRow.billing_days}
-                disabled
-              />
-            </div>
-          </Form.Row>
-          <Form.Row className="grid grid-cols-12 gap-5">
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.price}
-                name={"price"}
-                label={"Price"}
-                defaultValue={selectedRow.price}
-                disabled
-              />
-            </div>
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.orders_count}
-                name={"orders_count"}
-                label={"Orders count"}
-                defaultValue={selectedRow.orders_count}
-                disabled
-              />
-            </div>
-          </Form.Row>
-          <Form.Row className="grid grid-cols-12 gap-5">
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.balance}
-                name={"balance"}
-                label={"Balance"}
-                defaultValue={selectedRow.balance}
-                disabled
-              />
-            </div>
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.order_price}
-                name={"order_price"}
-                label={"Order price"}
-                defaultValue={selectedRow.order_price}
-                disabled
-              />
-            </div>
-          </Form.Row>
-          <Form.Row className="grid grid-cols-12 gap-5">
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.started_at}
-                name={"started_at"}
-                label={"Started at"}
-                defaultValue={selectedRow.started_at}
-                disabled
-              />
-            </div>
-            <div className="col-span-3 sm:col-span-6">
-              <TextInput
-                key={selectedRow.expire_at}
-                name={"expire_at"}
-                label={"Expire at"}
-                defaultValue={selectedRow.expire_at}
-                disabled
-              />
+            <div className="col-span-12 gap-5 w-[20rem]">
+              <div className="text-placeholder-color text-center col-span-1 sm:col-span-6 pt-2">
+                {subscription?.name}
+              </div>
+              <Form.Row className="cols-span-6">
+                <div className="col-span-1 sm:col-span-6 space-y-2 pt-5">
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Plan</span>
+                    <span>{subscription?.Plan?.name}</span>
+                  </div>
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Balance</span>
+                    <span className="text-end">{subscription.balance}</span>
+                  </div>
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Billing days</span>
+                    <span className="text-end">
+                      {subscription.billing_days}
+                    </span>
+                  </div>
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Price</span>
+                    <span className="text-end">{subscription.price}</span>
+                  </div>
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Orders count</span>
+                    <span className="text-end">
+                      {subscription.orders_count}
+                    </span>
+                  </div>
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Orders price</span>
+                    <span className="text-end">{subscription.order_price}</span>
+                  </div>
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Started at</span>
+                    <span className="text-end">{subscription.started_at}</span>
+                  </div>
+                  <div className="flex justify-between text-placeholder-color">
+                    <span>Expire at</span>
+                    <span className="text-end">{subscription.expire_at}</span>
+                  </div>
+                </div>
+              </Form.Row>
             </div>
           </Form.Row>
         </Form.Content>
